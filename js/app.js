663ddb46cd9d749d76b26b384685e99982e784e6
FlowerCake13 <jrdevleague19@Juniors-iMac.local>
2016-08-06 11:56:28 -1000

    High score
    high score

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 var Uscore = 0;
-
+var newGame = false;
 var highscore = 0;
+var prevScore;
 document.getElementById('hiscore').innerHTML = "New Highscore: " + highscore;
 var timerStart = false;
 
 var press = function() {
 	if (timerStart == false) {
+		// newGame=true;
 		coolerfunction();
 		timerStart = true;
 	};
+	if (newGame == true) {
+			Uscore = 0;
+			newGame = false;
+	}
 	Uscore++;
 	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
 
@@ -20,12 +26,16 @@ var press = function() {
 
 function coolerfunction() {
     setTimeout(function(){ 
+    	prevScore = Uscore;
 		var cps = Uscore/10;
 		alert("Your CPS is "+cps);
-		Uscore = highscore;
+		// Uscore++;
+		if (prevScore > highscore) {
+			highscore = Uscore;
+		}
 		// Uscore = 0;
 		document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+		newGame = true;
 		timerStart = false;
-
 	}, 10000);
 }
